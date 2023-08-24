1fe1d703b1f9a72d87af0f0822e14284ebebe393
qlthd93 <quentinlathaud@hotmail.fr>
2023-08-24 23:59:16 +0200

    Booking card example improved

--- components/BookingCard.tsx
+++ components/BookingCard.tsx
@@ -1,15 +1,19 @@
-import { Card } from '@rneui/base';
+import { AirbnbRating, Card } from '@rneui/base';
 import * as React from 'react'
+import { Image } from 'react-native';
 import { Text, View } from 'react-native';
 import tw from 'twrnc';
 
 export const BookingCard = () => {
-    
+    const imageUri = "https://media.routard.com/image/47/9/khao-sok-parc-thailande.1533479.jpeg";
     return (
         <Card containerStyle={tw`rounded-lg`}>
+            <Image source={{ uri : imageUri}} style={{width: '100%', height: '50%'}}/>
             <Text style={tw`font-bold text-xl`}>Booking name</Text>
             <Text style={tw`text-gray-400`}>Location</Text>
-
+            <AirbnbRating
+                isDisabled
+            />
         </Card>
     );
 }
