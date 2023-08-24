b49e7150e03fd02c5ad7df674d02a5b2d1489b3a
qlthd93 <quentinlathaud@hotmail.fr>
2023-08-24 15:16:36 +0200

    added react native elements package & searchbar

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,11 +1,23 @@
 import * as React from 'react'
 import { Text, View } from 'react-native';
+import { SearchBar } from '@rneui/themed';
+import { useState } from 'react';
 
 export const Home = () => {
- 
+  const [search, setSearch] = useState("");
+
+  const updateSearch = (search: any) => {
+    setSearch(search);
+  };
+  
   return (
     <View>
       <Text className="font-workSans text-3xl">Booking app</Text>
+      <SearchBar
+      placeholder="Search"
+      onChangeText={updateSearch}
+      value={search}
+      />
     </View>
   );
 }
