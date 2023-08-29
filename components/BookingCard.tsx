68dd6aad58b44c89b00699b9ab09c6793cede65c
qlthd93 <quentinlathaud@hotmail.fr>
2023-08-29 18:28:31 +0200

    Improved Booking card design and added heart icon

--- components/BookingCard.tsx
+++ components/BookingCard.tsx
@@ -1,29 +1,41 @@
-import { Card } from '@rneui/base';
+import { Button, Card, Icon } from '@rneui/base';
 import * as React from 'react'
 import { Image } from 'react-native';
 import { Text, View } from 'react-native';
 import tw from 'twrnc';
 import { Rating } from 'react-native-ratings';
+import { CardImage } from '@rneui/base/dist/Card/Card.Image';
+import { AntDesign } from "@expo/vector-icons";
 
 
 export const BookingCard = () => {
     const imageUri = "https://media.routard.com/image/47/9/khao-sok-parc-thailande.1533479.jpeg";
 
     return (
-        <Card containerStyle={tw`rounded-lg`}>
-            <Image source={{ uri : imageUri}} style={{width: '100%', height: '50%'}}/>
-            <Text style={tw`font-bold text-xl mt-2`}>Booking name</Text>
-            <Text style={tw`text-gray-400`}>Location</Text>
-            <View>
-                <View style={tw`flex flex-row my-2`}>
-                    <Rating
-                        imageSize={24}
-                        readonly
-                    />
-                    <Text style={tw`font-bold text-lg my-auto mx-2`}>3.1</Text>
+        <Card containerStyle={tw`rounded-lg p-0`}>
+            <CardImage source={{ uri : imageUri}} style={tw`rounded-t-lg`}/>
+            <View style={tw`p-4`}>
+                <View style={tw`flex flex-row justify-between`}>
+                    <View>
+                        <Text style={tw`font-bold text-xl mt-2`}>Booking name</Text>
+                        <Text style={tw`text-gray-400`}>Location</Text>
+                    </View>
+                    <AntDesign name="hearto" size={24} color="black" style={tw`mt-2`} />
                 </View>
-                <Text style={tw`text-gray-400`}>300 reviews</Text>
+                
+                <View style={tw`flex flex-row justify-between`}>
+                    <View style={tw`flex flex-row my-2`}>
+                        <Rating
+                            imageSize={24}
+                            startingValue={4.5}
+                            readonly
+                        />
+                        <Text style={tw`font-bold text-lg my-auto mx-2`}>4.5</Text>
+                    </View>
+                    <Text style={tw`text-gray-400 text-lg my-auto ml-2`}>300 reviews</Text>
+                </View>
             </View>
+            
         </Card>
     );
 }
--- components/Home.tsx
+++ components/Home.tsx
@@ -21,6 +21,8 @@ export const Home = () => {
         value={search}
       />
       <BookingCard/>
+      <BookingCard/>
+
     </SafeAreaView>
   );
 }
