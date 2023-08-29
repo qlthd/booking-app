32d76925fdce0c1297c8fd71647802b311fb40f5
qlthd93 <quentinlathaud@hotmail.fr>
2023-08-29 17:07:50 +0200

    Improvements booking card style

--- components/BookingCard.tsx
+++ components/BookingCard.tsx
@@ -1,19 +1,29 @@
-import { AirbnbRating, Card } from '@rneui/base';
+import { Card } from '@rneui/base';
 import * as React from 'react'
 import { Image } from 'react-native';
 import { Text, View } from 'react-native';
 import tw from 'twrnc';
+import { Rating } from 'react-native-ratings';
+
 
 export const BookingCard = () => {
     const imageUri = "https://media.routard.com/image/47/9/khao-sok-parc-thailande.1533479.jpeg";
+
     return (
         <Card containerStyle={tw`rounded-lg`}>
             <Image source={{ uri : imageUri}} style={{width: '100%', height: '50%'}}/>
-            <Text style={tw`font-bold text-xl`}>Booking name</Text>
+            <Text style={tw`font-bold text-xl mt-2`}>Booking name</Text>
             <Text style={tw`text-gray-400`}>Location</Text>
-            <AirbnbRating
-                isDisabled
-            />
+            <View>
+                <View style={tw`flex flex-row my-2`}>
+                    <Rating
+                        imageSize={24}
+                        readonly
+                    />
+                    <Text style={tw`font-bold text-lg my-auto mx-2`}>3.1</Text>
+                </View>
+                <Text style={tw`text-gray-400`}>300 reviews</Text>
+            </View>
         </Card>
     );
 }
