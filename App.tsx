d4c9e270babfe5b6129ed06bfb63879d2d8891e0
qlthd93 <quentinlathaud@hotmail.fr>
2023-08-29 22:22:57 +0200

    setup queryclient

--- App.tsx
+++ App.tsx
@@ -3,8 +3,11 @@ import { StyleSheet, Text, View } from 'react-native';
 import { useFonts } from "expo-font";
 import BottomTabs from "./components/BottomTabs";
 import { NavigationContainer } from '@react-navigation/native';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 const App = () => {
+  const queryClient = new QueryClient();
+
   const [loaded] = useFonts({
     WorkSansRegular: require("./assets/fonts/WorkSans-Regular.ttf"),
     SubjectivityRegular: require("./assets/fonts/Subjectivity-Regular.otf"),
@@ -15,9 +18,11 @@ const App = () => {
   }
 
   return (
-    <NavigationContainer>
-      <BottomTabs/>
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <BottomTabs/>
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
 
